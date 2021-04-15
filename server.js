6f3e9410de96636f2444f137742316347334825a
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-04-14 20:39:26 -0700

    added default 404

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
-const PORT = process.env.PORT || 4000;
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello World"
-    });
+// confirm Express.js connection
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "Hello World"
+//     });
+// });
+
+// Default response for any other request (Not Found)
+// Must be last route bc will override all others
+app.use((req, res) => {
+    res.status(404).end();
 });
 
 app.listen(PORT, () => {
