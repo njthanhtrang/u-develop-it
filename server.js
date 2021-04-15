6a141cb89ecf3eacb95bd47965acb797d74ec8a0
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-04-14 17:46:23 -0700

    port issue

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
